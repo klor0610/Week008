e30dc467790c520461980c11820a841564e51a4b
Kasia <Kasia1187@yahoo.com>
2018-03-05 20:27:34 -0600

    week 3 lab module

---
+++ week003.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#week3
+#dump blah blah into the null- output is disposed of
+echo "blah blah" > /dev/null
+cd /dev
+ls -l | less
+#shows block devices and file systems
+lsblk --fs
+#create an empty file
+cd ~; pwd; dd if=/dev/zero of=./empty.file bs=1M count=1
+#turns contents of file into hexidecimal
+hexdump -x empty.file
+#dmesg shows all kernel messages, grep sd will show all kernel
+#messages with sd
+dmesg | grep sd
