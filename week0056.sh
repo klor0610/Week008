19026949f0a862ee7491942bfef2e964b27e846e
Kasia <Kasia1187@yahoo.com>
2018-03-05 21:40:59 -0600

    week 5&6 module

---
+++ week0056.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#week 5 & 6 script review premidterm
+
+#shows kernel log
+less /var/log/kern.log
+#kernel boot parameters
+cat /proc/cmdline
+#find grub.cfg
+cd /boot
+ls
+cd grub
+less grub.cfg
+#generates a config file for grub.cfg
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+#install grub onto your usb
+sudo grub-install /dev/sdc
