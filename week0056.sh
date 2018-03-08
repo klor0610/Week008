6268eab261e5db512b672ca8755a2a11271170ee
klor0610 <35321148+klor0610@users.noreply.github.com>
2018-03-07 19:55:09 -0600

    Update Week0056.sh
    changed line 5 & 12, used | less

--- week0056.sh
+++ week0056.sh
@@ -2,14 +2,14 @@
 #week 5 & 6 script review premidterm
 
 #shows kernel log
-less /var/log/kern.log
+/var/log/kern.log | less
 #kernel boot parameters
 cat /proc/cmdline
 #find grub.cfg
 cd /boot
 ls
 cd grub
-less grub.cfg
+grub.cfg | less
 #generates a config file for grub.cfg
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 #install grub onto your usb
