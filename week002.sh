4a5a94f4b9358271a203b43d33798f7c1cf784e0
klor0610 <35321148+klor0610@users.noreply.github.com>
2018-03-12 23:24:08 -0500

    updated week002.sh
    made script user friendly to read its output.

--- week002.sh
+++ week002.sh
@@ -10,12 +10,17 @@ echo "IL">>week_002.txt
 echo "MN">week_022.txt
 echo "NM">>week_022.txt
 echo "AL">>week_022.txt
-#show difference between the two fie
+#show difference between the two files
+sleep 3s
+echo "Here are the differences of two files, week_002.txt and week_022.txt"
 diff week_002.txt week_022.txt
 #search for similarities in the two files, option i allows upper and
 #lower case search
+sleep 2s
+echo "let's try searching for m in week_002.txt"
+sleep 2s
 grep -i m  week_002.txt
-echo "hit enter to continue"
+echo "hit enter to sort file week_002.txt in alphabetical order"
 read enter
 #sort both files
 sort -d week_002.txt
