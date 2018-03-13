53707a1b16a4adf0ff233fc3b433a6f5ee5ae750
klor0610 <35321148+klor0610@users.noreply.github.com>
2018-03-12 23:50:17 -0500

    updated week 004.sh
    entered sleep commands

--- week004.sh
+++ week004.sh
@@ -2,18 +2,23 @@
 #week 4 scripts - midterm review
 
 #shows manual of udevadm (udev management tool)
+echo "lets take a look at the manual pages for udevadm"
+sleep 4s
 man udevadm
 #shows partition table
 sudo fdisk -l /dev/sda
+sleep 4s
 #create a partition & filesystem
 dd if=/dev/zero of=./12MB.img bs=1M count=12
 hexdump ./12MB.img | less
+sleep 3s
 #make the filesystem ext4
 mkfs -t ext4 ./12MB.img
 #create mount point
 sudo mkdir /media/tmp2
 sudo mount ./12MB.img /media/tmp2
 mount; df -h
+sleep 2s
 lsblk
 #input content into file
 cd /media/tmp2
