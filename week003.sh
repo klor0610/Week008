d74a7b221ac9a87156a25fc0ca4e1de19423cb33
klor0610 <35321148+klor0610@users.noreply.github.com>
2018-03-12 23:31:57 -0500

    Update week003.sh

--- week003.sh
+++ week003.sh
@@ -4,12 +4,17 @@
 echo "blah blah" > /dev/null
 cd /dev
 ls -l | less
+echo "press enter to see block devices"
+reach enter
 #shows block devices and file systems
 lsblk --fs
+sleep 5s
 #create an empty file
 cd ~; pwd; dd if=/dev/zero of=./empty.file bs=1M count=1
 #turns contents of file into hexidecimal
+sleep 5s
 hexdump -x empty.file
 #dmesg shows all kernel messages, grep sd will show all kernel
 #messages with sd
+sleep 5s
 dmesg | grep sd
